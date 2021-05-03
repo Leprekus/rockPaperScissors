e5cc4ed1c16b74e6524567af41c2e6db990fdf09
Leprekus <leprekus@yahoo.com>
2021-05-03 13:17:47 -0500

    html now has icons for the three options, also added functions in js for the computer to return something and the player can only lose at the moment by choosing rock

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,38 @@
-let computerPlay = function(move){
+const OUTPUT=document.querySelector('.output')
+
+const ROCK=document.querySelector('#rock')
+const PAPER=document.querySelector('#paper')
+const SCISSORS=document.querySelector('#scissors')
+
+
+
+let rock = '🪨';
+let paper = '📃';
+let scisssors = '✂';
+
+let computerPlay = function(){
     let move = ['🪨', '📃', '✂'];
     return move[Math.floor(Math.random()*move.length)];
 
 }
-console.log(computerPlay(move));
+
+function game(playerSelection, computerSelection, turns){
+
+  // define the function and call it on the do... while... loop
+  let playRound = function(){ 
+  if (playerSelection === '🪨' && computerSelection ==='📃'){
+    return ('Player: ' + playerSelection + ' Computer: ' 
+             + computerSelection + ' You lose Paper beats Rock! :(');
+  }else{
+    return 'Computer Chose: ' + computerSelection;
+  }
+  };
+
+  for (let i= 0; i < turns; i++){
+    return playRound();
+  }
+
+};
+console.log(computerPlay());
+console.log(game(rock, computerPlay(), 5));
 
